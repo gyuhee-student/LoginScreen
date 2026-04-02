9b51c597ac5f34ebf1c1770410ef53af7a58563f
황규희 <gyuhee3021@gmail.com>
2026-04-02 16:37:03 +0900

    기본 ui 구현 및 로그인 기능 구현.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            lblAppName = new Label();
+            txtID = new TextBox();
+            txtPW = new TextBox();
+            btnLogin = new Button();
+            SuspendLayout();
+            // 
+            // lblAppName
+            // 
+            lblAppName.AutoSize = true;
+            lblAppName.Font = new Font("Engravers MT", 48F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblAppName.ForeColor = Color.FromArgb(255, 128, 0);
+            lblAppName.Location = new Point(71, 26);
+            lblAppName.Name = "lblAppName";
+            lblAppName.Size = new Size(617, 149);
+            lblAppName.TabIndex = 1;
+            lblAppName.Text = "Login";
+            lblAppName.Click += ｌｂｌ_Click;
+            // 
+            // txtID
+            // 
+            txtID.Font = new Font("맑은 고딕", 28.125F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            txtID.ForeColor = Color.FromArgb(224, 224, 224);
+            txtID.Location = new Point(107, 235);
+            txtID.Name = "txtID";
+            txtID.Size = new Size(539, 107);
+            txtID.TabIndex = 2;
+            txtID.Text = "아이디";
+            txtID.FontChanged += txtID_FontChanged;
+            txtID.Enter += txtID_Enter;
+            txtID.Leave += txtID_Leave;
+            // 
+            // txtPW
+            // 
+            txtPW.Font = new Font("맑은 고딕", 28.125F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            txtPW.ForeColor = Color.FromArgb(224, 224, 224);
+            txtPW.Location = new Point(107, 415);
+            txtPW.Name = "txtPW";
+            txtPW.Size = new Size(539, 107);
+            txtPW.TabIndex = 3;
+            txtPW.Text = "패스워드";
+            txtPW.TextChanged += txtPW_TextChanged;
+            txtPW.Enter += txtPW_Enter;
+            txtPW.Leave += txtPW_Leave;
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = Color.FromArgb(192, 255, 255);
+            btnLogin.Font = new Font("휴먼엑스포", 28.125F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btnLogin.ForeColor = Color.Teal;
+            btnLogin.Location = new Point(188, 611);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(345, 129);
+            btnLogin.TabIndex = 0;
+            btnLogin.Text = "로그인\r\n";
+            btnLogin.UseVisualStyleBackColor = false;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(14F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            ClientSize = new Size(765, 820);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPW);
+            Controls.Add(txtID);
+            Controls.Add(lblAppName);
+            Name = "Form1";
+            Text = "Login Screen";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblAppName;
+        private TextBox txtID;
+        private TextBox txtPW;
+        private Button btnLogin;
     }
 }
